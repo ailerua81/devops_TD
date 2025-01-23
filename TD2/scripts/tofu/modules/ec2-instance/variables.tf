6e267c9e8f5223a7fe9c4b201a83bf8adb80be1f
ailerua81 <aurelia.pesquet@edu.esiee.fr>
2025-01-23 10:59:41 +0100

    Update variables.tf

--- TD2/scripts/tofu/modules/ec2-instance/variables.tf
+++ TD2/scripts/tofu/modules/ec2-instance/variables.tf
@@ -16,14 +16,14 @@ variable "ami_id" {
 # OPTIONAL PARAMETERS
 # ---------------------------------------------------------------------------------------------------------------------
 
-#variable "instance_type" {
-#  description = "The type of EC2 instance to deploy (e.g., t2.micro)"
-#  type        = string
-#  default     = "t2.micro"
-#}
+variable "instance_type" {
+  description = "The type of EC2 instance to deploy (e.g., t2.micro)"
+  type        = string
+  default     = "t2.micro"
+}
 
-#variable "http_port" {
-#  description = "The port the EC2 instance will listen on for HTTP requests"
-#  type        = number
-#  default     = 8080
-#}
+variable "http_port" {
+  description = "The port the EC2 instance will listen on for HTTP requests"
+  type        = number
+  default     = 8080
+}
