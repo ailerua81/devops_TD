52045b8eb4267cf61446488a9b5f7badf920caa6
ailerua81 <aurelia.pesquet@edu.esiee.fr>
2025-02-06 22:33:00 +0100

    Update main.tf

--- td5/scripts/tofu/live/tofu-state/main.tf
+++ td5/scripts/tofu/live/tofu-state/main.tf
@@ -6,5 +6,5 @@ module "state" {
   source = "github.com/ailerua81/devops_TD//td5/scripts/tofu/modules/state-bucket"
 
   # TODO: fill in your own bucket name!
-  name = "devops_td5_bucket_name"
+  name = "ap-devops-td5-bucket-name"
 }
